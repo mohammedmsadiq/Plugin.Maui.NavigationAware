1aa8cb2b2682c2f0024fc05daad3e6567025adaa
Mohammed Sadiq <>
2025-10-06 17:03:26 +0400

    no message

--- src/Plugin.Maui.NavigationAware/IFeature.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Plugin.Maui.NavigationAware;
-
-/// <summary>
-/// Legacy interface - use INavigationAware instead
-/// </summary>
-[Obsolete("This interface is deprecated. Use INavigationAware and related interfaces instead.")]
-public interface IFeature
-{
-}
